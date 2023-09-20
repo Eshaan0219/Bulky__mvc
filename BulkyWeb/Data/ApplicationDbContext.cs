c78cd1556bf750899204ce3bbabc38f4d4c32c9a
Eshaan Chanda <eshaan0@hotmail.com>
2023-09-20 13:15:12 +0530

    hello

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Explorer> Explorer { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { id = 3, Name = "History", DisplayOrder = 3 }
+                );
+            modelBuilder.Entity<Explorer>().HasData(
+                new Explorer { Id = 1, Name = "Action" },
+                new Explorer { Id = 2, Name = "Sci-Fi"},
+                new Explorer { Id = 3, Name = "History" }
+                );
+        }
+        
+        
+    }
+}
---
+++ BulkyWeb/Migrations/20230907115034_newexp.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class newexp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Explorer",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Action" },
+                    { 2, "Sci-Fi" },
+                    { 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Explorer",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Explorer",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Explorer",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
